4df80042bb03a817bfc994a4a8a376ba14f834a0
CJ Chaney <cj@cjchaney.com>
2011-12-28 06:47:00 +0000

    First commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <script>
+            function test(name) {
+                alert(name);
+            }
+        </script>
+    </head>
+    <body>
+        This is the wedding app home page
+    </body>
+</html>
